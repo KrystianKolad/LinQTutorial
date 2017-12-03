a3bd9a40f3129a32c03197602348b8b5b5c1ba47
Krystian <krystiankolad@wp.pl>
2017-12-03 14:09:54 +0100

    Added PLinq Example

--- src/Linq/Program.cs
+++ src/Linq/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
-namespace Linq
-{
-    class Program
-    {
-        static void Main(string[] args)
+namespace Linq {
+    class Program {
+        static void Main (string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PLinqExample(100000000);
+            PLinqExample(1000000);
+            Console.ReadKey();
+        }
+
+        private static void PLinqExample (int range) 
+        {
+            Console.WriteLine("Wynik działania programu dla " + range + " Liczb:");
+            IEnumerable<int> data = Enumerable.Range(1, range);
+            int condition = range/10;
+
+            var lquery = data.Where (x => x % condition == 0);
+
+            Console.WriteLine ("LinQ result:");
+            foreach (var item in lquery) {
+                Console.WriteLine (item);
+            }
+
+            var pquery = data.AsParallel().Where (x => x % condition== 0);
+
+            Console.WriteLine ("PlinQ result:");
+            foreach (var item in pquery) {
+                Console.WriteLine (item);
+            }
         }
     }
 }
